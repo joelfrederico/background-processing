d2d852472654771a5fa3b4490b6bf1f6f2e1e32a
Joel Frederico <joelfred@slac.stanford.edu>
2013-06-13 18:45:54 -0700

    Turned into a function.

--- bg_sub.m
+++ bg_sub.m
@@ -1,22 +1,12 @@
-function data=bg_sub()
+function data=bg_sub(data)
 	% Make sure E200_data is on my path.
 	addpath('../../../');
 
-	% Path to the dataset I want.
-	% Note that it doesn't need the prefix to the location of /nas anymore!
-	% path='/nas/nas-li20-pm01/E200/2013/20130428/E200_10794';
-	path='/nas/nas-li20-pm01/E200/2013/20130428/E200_10836';
-	% path='nas/nas-li20-pm01/E200/2013/20130514/E200_11159';
-	
-	% Load this data.
-	data=E200_load_data(path);
-	
 	% Loop over images
-	image_name_str=fieldnames(data.raw.images)
+	image_name_str=fieldnames(data.raw.images);
 	for i=1:size(image_name_str)
 		% Get image name
 		imgname=image_name_str{i};
-		display(imgname);
 
 		% Loop over steps
 		steps=unique(data.raw.scalars.step_num.dat);
@@ -35,7 +25,6 @@ function data=bg_sub()
 			img=cell2mat(img);
 			bg=cell2mat(bg);
 			sub=reshape(img-uint16(bg),xres,yres,img_num);
-			size(sub)
 			toc;
 			img_proc=cat(3,img_proc,sub);
 		end
@@ -47,40 +36,4 @@ end
 
 function data=add_processed(data,varargin)
 	% data.processed.images.(name).
-end
-
-function old()
-	% I want to load data from YAG.
-	imgstruct=data.raw.images.YAG;
-
-	% I want to load only images from the 2nd step
-	bool1=(data.raw.scalars.step_num.dat==1);
-
-	% I want the UIDs of the 2nd step
-	wanted_UIDs = data.raw.images.YAG.UID(bool1);
-
-	% Load those images
-	% Note: The third argument, data, is optional.
-	%	But including it allows E200_load_images
-	%	to determine if the images are saved
-	%	remotely or locally.
-	display(['Loading ' num2str(2*size(wanted_UIDs,2)) ' images ...']);
-	tic;
-	[img,bg]=E200_load_images(imgstruct,wanted_UIDs,data);
-	toc;
-	display(sprintf('\n'));
-	
-	% Loop over my images
-	% Note: only show first 5.
-	display('Click on the image to continue.');
-	for i=1:5
-		display(['Image ' num2str(i) '...']);
-		% Plot the image
-		imagesc(img{i}-uint16(bg{i}));
-		% Wait for the user to press a key
-		waitforbuttonpress;
-	end
-
-	% Close the figures.
-	close('all');
 end
